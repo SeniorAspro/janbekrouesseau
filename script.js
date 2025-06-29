2205c4deb0e5c92d4a344ef3db9eb0cc6b6b083a
Yekta Çifteler <yektacifteler@gmail.com>
2025-06-29 11:40:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const arrivalTime = new Date("July 1, 1999, 12:00:00");
+    const today = new Date();
+
+    const arrive = arrivalTime.getTime();
+
+    arrive = arrive.setTime
+
+  });
